8888bf3962585c8d3dd3becef38d278772b650ff
zaki <zaki@localhost>
2023-04-10 00:04:33 -0700

    z

---
+++ src/day_2_project_5/day_5.java
@@ -0,0 +1,10 @@
+package day_2_project_5;
+
+public class day_5 {
+		public static void main(String[] args) {
+			System.out.println("jasdflalskfj");
+		}
+		public void s() {
+			System.out.println("aslfj");
+		}
+}
